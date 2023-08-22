2d59295280daf0b42cf1481cc01139e96ee2a0ac
shale <slucas001@citymail.cuny.edu>
2023-08-21 21:56:42 -0400

    added open and closeDB methods

--- src/BookManager.java
+++ src/BookManager.java
@@ -3,14 +3,45 @@ import java.sql.DriverManager;
 import java.sql.SQLException; 
 
 // TODO
-//  - import the SQL code
-//  - import the MariaDB things
-//  - establish connection with the database 
+//  - Establish connection with the database 
+//  - Probably install MariaDB database driver instead of "DriverManager"
+//  - Write a constructor? Let the compiler do it? 
 
 public class BookManager {	
 	
+	private static Connection conn; 
+
+	public static void openDbConnection(String url) {
+		System.out.println("Establishing a connection to the DB...");
+		try {
+			conn = DriverManager.getConnection(url); 
+
+		} catch (SQLException ex){
+			System.err.println("SQL State:" + ex.getSQLState()); 
+			System.out.println("Message:" + ex.getMessage()); 
+		}
+	}
+
+	public static void closeDbConnection() {
+		System.out.println("Closing the DB connection"); 
+		try {
+			conn.close(); 
+
+		} catch (SQLException ex){
+			System.err.println("SQL State:" + ex.getSQLState()); 
+			System.out.println("Message:" + ex.getMessage()); 
+		}
+	}
+
+	
 	public static void main(String[] args){
-		System.out.println("Testing"); 
+		BookManager bm = new BookManager(); 
+		String url = "jdbc:mariadb://localhost:3306/BookKeeper"; 
+		bm.openDbConnection(url); 
+
+		// DO SQL statement things...
+	
+		bm.closeDbConnection();
 	}
 
 }
