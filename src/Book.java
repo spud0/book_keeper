b3dce4d9760866c940ee8e3b32e5ffae3531da68
shale <slucas001@citymail.cuny.edu>
2023-08-21 21:02:59 -0400

    added the src/ directory

---
+++ src/Book.java
@@ -0,0 +1,69 @@
+// TODO: 
+// - Think about adding support for Images. 
+
+// Book datatype. Contains important fields about a book. 
+public class Book {
+
+	// All books have these pieces of data.
+	public String AuthorName; 
+	public String BookTitle; 
+	public long PageCount; 
+	public long TimesRead; 
+	
+	// The Constructor of the class. 
+	public Book(String AuthorName, String BookTitle, long PageCount){
+		this.AuthorName = AuthorName; 
+		this.BookTitle = BookTitle; 
+		this.PageCount = PageCount; 
+	}
+
+	// The Getter methods.  
+	public String getAuthorName(){
+		return AuthorName; 
+	}
+
+	public String getBookTitle(){
+		return BookTitle; 
+	}
+
+	public long getPageCount(){
+		return PageCount; 
+	}
+
+	public long getTimesRead(){
+		return TimesRead;
+	}
+	
+	// The Setter methods. Use this to change any of the fields.
+	public void setTimesRead(long TimesRead){
+		this.TimesRead = TimesRead; 
+	}
+	
+	public void setAuthorName (String AuthorName){
+		this.AuthorName = AuthorName; 
+	}
+
+	public void setBookTitle(String BookTitle){
+		this.BookTitle = BookTitle; 
+	}
+
+	public void setPageCount(String PageCount){
+		this.PageCount = PageCount; 
+	}
+
+	// Main method. 
+	public static void main(String[] args){
+		String an = "Malcolm Gladwell"; 
+		String bt = "Outliers"; 
+		long pgct = 450;
+
+		Book b = new Book(an, bt, pgct); 
+
+		System.out.println(b.getAuthorName());
+		System.out.println(b.getBookTitle()); 
+		System.out.println(b.getPageCount()); 
+		b.setTimesRead(10);
+		System.out.println(b.getTimesRead()); 
+	} 	
+}
+
---
+++ src/BookManager.java
@@ -0,0 +1,16 @@
+import java.sql.Connection; 
+import java.sql.DriverManager; 
+import java.sql.SQLException; 
+
+// TODO
+//  - import the SQL code
+//  - import the MariaDB things
+//  - establish connection with the database 
+
+public class BookManager {	
+	
+	public static void main(String[] args){
+		System.out.println("Testing"); 
+	}
+
+}
