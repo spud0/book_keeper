5ab685df4e31bf58a4dd1efddbb854403c151d2c
shale <slucas001@citymail.cuny.edu>
2023-08-23 17:51:42 -0400

    fixed the errors in the sql code

--- db/db.sql
+++ db/db.sql
@@ -1,34 +1,32 @@
--- Created the User Table for the DB.
-CREATE TABLE IF NOT EXISTS Users (
-	UserID INT AUTO_INCREMENT NOT NULL,
-	Email VARCHAR(255) NOT NULL UNIQUE, 
-	Name TEXT NOT NULL, 
-	UName VARCHAR(255) NOT NULL, 
-	Pass TEXT NOT NULL,
-	PRIMARY KEY(UserID)
 
-); 
+-- Make the DB
+CREATE DATABASE UserBooks; 
+USE UserBooks; 
 
--- Created the Books Table for the DB. 
-CREATE TABLE IF NOT EXISTS Books(
-	OwnerID INT DEFAULT 0,
-	Author TEXT NOT NULL, 
-	Title  TEXT NOT NULL, 
-	PageCt  INT NOT NULL,
-	Times   INT NOT NULL, 
-	
-	-- This key relates the Users to the books that they own. 
-	CONSTRAINT 
-		FOREIGN KEY (OwnerID) REFERENCES Users(UserID) 
-		ON DELETE CASCADE
+-- The Table for the Users. 
+CREATE TABLE `Users`(
+    `ID` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `User Email` VARCHAR(255) NOT NULL,
+    `User Name` VARCHAR(255) NOT NULL,
+    `Name` LONGTEXT NOT NULL,
+    `Password` VARCHAR(255) NOT NULL
+);
 
-); 
+-- Make sure the User Email and User Name are unique
+ALTER TABLE
+    `Users` ADD UNIQUE `users_user email_unique`(`User Email`);
+ALTER TABLE
+    `Users` ADD UNIQUE `users_user name_unique`(`User Name`);
 
+-- The Table for the Books
+CREATE TABLE `Books`(
+    `Author Name` LONGTEXT NOT NULL,
+    `Book Title` LONGTEXT NOT NULL,
+    `Page Count` BIGINT NOT NULL,
+    `Times Read` BIGINT NOT NULL,
+    `ID` BIGINT NOT NULL
+);
 
-INSERT INTO Books(Author, Title, PageCt, Times)
-VALUES (
-	'Plato', 
-	'The Republic', 
-	350,
-	2
-); 
+-- The User's ID field will reference 1 or more books. 
+ALTER TABLE
+    `Books` ADD CONSTRAINT `books_id_foreign` FOREIGN KEY(`ID`) REFERENCES `Users`(`ID`);
