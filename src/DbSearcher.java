5ddad3049c1ec27fb4e75f2f78eed961586fdf52
shale <slucas001@citymail.cuny.edu>
2023-08-26 23:09:24 -0400

    added the getUserBooks method

--- src/DbSearcher.java
+++ src/DbSearcher.java
@@ -42,5 +42,47 @@ public class DbSearcher {
 			}	
 		}
   
+  
+	public static ResultSet getUsersBooks (String UserName, int Id) { 
+		
+		// Use a WHERE Statement based on the Id and UserName. 
+		Statement stmt = null;
+		ResultSet rs = null; 
+
+		try {
+			stmt = conn.createStatement(); 
+			String query = " SELECT `Author Name`, `Book Title`, `Page Count`, `Times Read` FROM `Books` WHERE `ID` = %s; "; 
+
+			if (stmt.execute(query) {
+				rs = stmt.getResultSet(); 
+				return rs; 
+			}
+		} catch (SQLException ex) {
+			// Print the error message.
+			System.out.println("SQLException: " + ex.getMessage());
+		    	System.out.println("SQLState: " + ex.getSQLState());
+		    	System.out.println("VendorError: " + ex.getErrorCode());
+
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close(); 
+				} catch (SQLException ex) { } // Do nothing. 
+				rs = null; 
+			}
+			if (stmt != null) {
+				try {
+					stmt.close(); 
+				} catch (SQLException ex) { } // Do nothing. 
+				stmt = null; 
+			}
+		}
+
+
+	public static String getUserPass (ResultSet Rs) {
+    return Rs.getString("Password"); 
+	}
+
+
 }
 
