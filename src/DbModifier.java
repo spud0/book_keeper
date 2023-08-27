efe07eb572c03c1c53d94e5232dd3cdbb629fae3
shale <slucas001@citymail.cuny.edu>
2023-08-26 22:58:06 -0400

    removed the getUserInfo method

--- src/DbModifier.java
+++ src/DbModifier.java
@@ -13,41 +13,6 @@ public class DbModifier {
 	}
 
 
-	public static ResultSet getUserInfo (String UserName) {
-		
-		Statement stmt = null; 
-		ResultSet rs = null; 
-
-		try {
-			stmt = conn.createStatement(); 
-			String query = " SELECT 'User Email', 'User Name', Name, ID FROM Users WHERE 'User Name' = %s; "; 
-			String q = String.format(query, UserName); 
-
-			if (stmt.execute(q) {
-				rs = stmt.getResultSet(); 
-				return rs; 
-			}
-
-		} catch (SQLException ex) {
-			// Print the error message.
-			System.out.println("SQLException: " + ex.getMessage());
-		 	System.out.println("SQLState: " + ex.getSQLState());
-		  System.out.println("VendorError: " + ex.getErrorCode());
-
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close(); 
-				} catch (SQLException ex) { } // Do nothing. 
-				rs = null; 
-			}
-			if (stmt != null) {
-				try {
-					stmt.close(); 
-				} catch (SQLException ex) { } // Do nothing. 
-			}	
-		}
-	}
 
 	public static ResultSet getUsersBooks (String UserName, int Id) { 
 		
