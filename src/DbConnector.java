350a8d0205cfb9589f39932a5c3b6bfe0b741cb3
shale <slucas001@citymail.cuny.edu>
2023-08-28 19:50:08 -0400

    added the getUserInfo method

--- src/DbConnector.java
+++ src/DbConnector.java
@@ -35,7 +35,46 @@ public class DbManager {
 			}
 		}
 	}
+  
 
+  // Searching methods for the DB. 
+  
+	public static ResultSet getUserInfo (String UserName, Connection c) {
+		
+		Statement stmt = null; 
+		ResultSet rs = null; 
+
+		try {
+			stmt = c.createStatement(); 
+			String query = " SELECT 'User Email', 'User Name', Name, ID FROM Users WHERE 'User Name' = %s; "; 
+			String q = String.format(query, UserName); 
+
+			if (stmt.execute(q)) {
+				rs = stmt.getResultSet(); 
+				return rs; 
+			}
+
+		} catch (SQLException ex) {
+			// Print the error message.
+			System.out.println("SQLException: " + ex.getMessage());
+		 	System.out.println("SQLState: " + ex.getSQLState());
+		  System.out.println("VendorError: " + ex.getErrorCode());
+
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close(); 
+				} catch (SQLException ex) { } // Do nothing. 
+				rs = null; 
+			}
+			if (stmt != null) {
+				try {
+					stmt.close(); 
+				} catch (SQLException ex) { } // Do nothing. 
+			}	
+		}
+    return rs; 
+  }
 
   public static void main(String[] args){
 		try {	
