e4869956245fb192a367f2f2bda14ac1de027347
shale <slucas001@citymail.cuny.edu>
2023-08-27 20:49:54 -0400

    removed commented out code

--- src/DbConnector.java
+++ src/DbConnector.java
@@ -44,66 +44,6 @@ public class DbManager {
 		} catch (Exception x){
 			System.err.println(x.getMessage()); 
 		}
-  /** 
-    DbConnector d = new DbConnector(); 
-
-		String url = "jdbc:mysql://localhost:3306/UserBooks";
-		String user = "root"; 
-		String pass = ""; 
-
-	  d.openDbConnection(url, user, pass); 
-
-		// DO SQL statement things...
-    Statement s = null; 
-    ResultSet r = null; 
-
-		try {
-      s = conn.createStatement(); 
-			String query = "SELECT * FROM `Users`";
-
-			if (s.execute(query)) {
-				r = s.getResultSet(); 
-			}
-
-      // handle the ResultSet
-      //
-      while (r.next()){
-
-        int id = r.getInt("ID"); 
-        String un = r.getString("User Name"); 
-        String n = r.getString("Name"); 
-        String p = r.getString("Password"); 
-        String e = r.getString("User Email");  
-
-        System.out.println("id: " + id); 
-        System.out.println("Uname: " + un); 
-        System.out.println("Name: " + n);
-        System.out.println("Pass: " + p); 
-        System.out.println("Email: " + e); 
-      }
-
-      
-		} catch (SQLException ex) {
-			// Print the error message.
-			System.out.println("SQLException: " + ex.getMessage());
-		  System.out.println("SQLState: " + ex.getSQLState());
-			System.out.println("VendorError: " + ex.getErrorCode());
-
-		} finally {
-			if (s != null) {
-				try {
-					s.close(); 
-				} catch (SQLException ex) { } // Do nothing. 
-				s = null; 
-			}
-
-      if (r != null){
-        try{
-          r.close(); 
-        } catch (SQLException ex) {}
-      }
-		}
-	
-		d.closeDbConnection(); 
-  **/ } 
+    
+  } 
 } 
