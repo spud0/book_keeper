861de893f3ccde383e34289ed9ae27fae98b3b53
shale <slucas001@citymail.cuny.edu>
2024-08-08 23:34:23 -0400

    added auto methods from spring boot for querying db

--- src/main/java/com/bookhaven/book_keeper/BookRepository.java
+++ src/main/java/com/bookhaven/book_keeper/BookRepository.java
@@ -1,11 +1,19 @@
 package com.bookhaven.book_keeper;
 
-// import org.bson.types.ObjectId;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-}
+	// All books for a particular author	
+ 	public List<Book> findByAuthorName(String authorName);
 
+	// All books for a particular publisher
+ 	public List<Book> findByPublisherName(String publisherName);
+
+
+}
