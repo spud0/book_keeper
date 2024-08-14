12c313a79474d8f1719907162676d274f75085c1
shale <slucas001@citymail.cuny.edu>
2024-08-13 23:46:00 -0400

    prototype of using isbn code to search for books, don't think it works

--- src/main/java/com/bookhaven/book_keeper/BookController.java
+++ src/main/java/com/bookhaven/book_keeper/BookController.java
@@ -17,7 +17,7 @@ public class BookController {
 	@Autowired
 	private BookService bookService;
 
-	// testing
+	// Gets all the books stored in the DB... 
 	@GetMapping
 	public ResponseEntity<List<Book>> getAllBooks () {	
 		return new ResponseEntity<List<Book>> (bookService.allBooks(), HttpStatus.OK);
@@ -33,5 +33,25 @@ public class BookController {
 	@GetMapping("/authors/{authorName}")	
 	public ResponseEntity<List<Book>> getAllBooksForAuthor (@PathVariable String authorName) {
 		return new ResponseEntity<List<Book>> (bookService.allBooksForAuthor(authorName), HttpStatus.OK);
+	}
+	
+	// Returns the unique book based on its ISBN
+	@GetMapping("/isbn/{isbnCode}")
+	public ResponseEntity<Book> getBookFromIsbnCode (@PathVariable String isbnCode) {
+		return new ResponseEntity<Book> (bookService.bookFromIsbnCode(isbnCode), HttpStatus.OK);
+	}
+
+	/* I Should probably move this to a commentService class */
+
+	// Based on the ISBN code get all the reviews associated with a book
+	@GetMapping("/isbn/{isbnCode}/reviews")
+	public ResponseEntity<Book> getReviewsForBook (@PathVariable String isbnCode) {
+		return new ResponseEntity<Book> (commentService.bookReviewsFromIsbnCode(isbnCode), HttpStatus.OK);
+	}
+
+	// Based on the ISBN code get all the reviews associated with a publisher
+	@GetMapping("/isbn/{isbnCode}/reviews")
+	public ResponseEntity<Book> getReviewsForBook (@PathVariable String isbnCode) {
+		return new ResponseEntity<Book> (commentService.publisherReviewsFromIsbnCode(isbnCode), HttpStatus.OK);
 	}
 }
